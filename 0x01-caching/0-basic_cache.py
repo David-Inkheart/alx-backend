44234952391f1fa1c5d56386bc181ff7e74b1c59
David-Inkheart <okoliedave1@gmail.com>
2023-05-16 18:04:40 +0100

    implementing a FIFO caching system

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -15,7 +15,7 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        """assign to the dictionary self.cache_data the item 
+        """assign to the dictionary self.cache_data the item
         value for the key key"""
         if key or item is not None:
             self.cache_data[key] = item
@@ -23,6 +23,6 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """return the value in self.cache_data
         linked to key"""
-        if not key or not key in self.cache_data:
+        if not key or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,7 +16,7 @@ class FIFOCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """assign to the dictionary self.cache_data the item 
+        """assign to the dictionary self.cache_data the item
         value for the key key"""
         if key or item is not None:
             self.cache_data[key] = item
@@ -28,6 +28,6 @@ class FIFOCache(BaseCaching):
     def get(self, key):
         """return the value in self.cache_data
         linked to key"""
-        if not key or not key in self.cache_data:
+        if not key or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
