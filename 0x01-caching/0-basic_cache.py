74c051bdb5a1dca784c66ad8d54a5ea6664a1436
David-Inkheart <okoliedave1@gmail.com>
2023-05-16 18:09:03 +0100

    Basic caching system without eviction policy

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,10 +10,6 @@ class BasicCache(BaseCaching):
     """A catching system that inherits from BaseCaching and has
     no limits."""
 
-    def __init__(self):
-        """contructor calling parent class constructor"""
-        super().__init__()
-
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item
         value for the key key"""
