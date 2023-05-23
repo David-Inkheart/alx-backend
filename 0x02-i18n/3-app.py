619ec8684fce7f4f5ad41645b9247604188d4946
David-Inkheart <okoliedave1@gmail.com>
2023-05-23 12:58:56 +0100

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """ Config class for Babel object """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ get locale from request """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+# the decorator to mark the function get_locale as a local selector
+# does not work with later versions of Flask-Babel so the following is used
+# babel.init_app(app, locale_selector=get_locale)
+
+
+# list of routes of my application
+@app.route('/', strict_slashes=False)
+def index():
+    """ Returns: 2-index.html """
+    header = "Hello world"
+    return render_template('2-index.html', header=header)
+
+
+if __name__ == '__main__':
+    """ Main Function"""
+    app.run()
