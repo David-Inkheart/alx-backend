696fa0318ee2d0f56501f85ca4ae9d714f8e6033
David-Inkheart <okoliedave1@gmail.com>
2023-05-23 15:34:39 +0100

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -23,12 +23,9 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# the decorator to mark the function get_locale as a local selector
-# does not work with later versions of Flask-Babel so the following is used
 # babel.init_app(app, locale_selector=get_locale)
 
 
-# list of routes of my application
 @app.route('/', strict_slashes=False)
 def index():
     """ Returns: 3-index.html """
